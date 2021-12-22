2cbc2cd08cce25696001064995da3dad3f4a063a
Mnkydputra <murryfebriansyah1@gmail.com>
2021-12-22 10:04:10 +0700

    penambahan struktur database

--- application/controllers/Danru/Dashboard.php
+++ application/controllers/Danru/Dashboard.php
@@ -3,7 +3,6 @@
 
 class Dashboard extends CI_Controller
 {
-<<<<<<< HEAD
     function __construct()
     {
       parent::__construct();
@@ -31,29 +30,4 @@ class Dashboard extends CI_Controller
         $this->load->view('mobile/fotter');
     }
 
-=======
-  function __construct()
-  {
-    parent::__construct();
-
-    $id = $this->session->userdata('id_akun');
-
-    if ($id == null || $id == "") {
-      $this->session->set_flashdata('info', 'sessi berakhir silahkan login kembali');
-      redirect('Login');
-    }
-  }
-
-  function index()
-  {
-    $data = array(
-      'biodata' => $this->db->get_where('biodata', array('id_biodata' => $this->session->userdata('id_akun')))->row(),
-      'url'  => $this->uri->segment(2),
-      'berkas'    => $this->db->get_where('berkas', array('id_berkas' => $this->session->userdata('id_akun')))->row(),
-    );
-    $this->load->view('mobile/header', $data);
-    $this->load->view('Danru/dashboard', $data);
-    $this->load->view('mobile/fotter');
-  }
->>>>>>> 68b03c13ee60cafcdf371d95d0f066dab906151a
 }
